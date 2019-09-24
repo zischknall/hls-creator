cc9a4c0d563de917621cb5968b2d1606d033d518
Tobias Rath <tobias.rath@protonmail.com>
2019-09-24 15:36:26 +0200

    stop script from deleting keyfile

--- hls-creator.sh
+++ hls-creator.sh
@@ -33,8 +33,6 @@ ffmpeg -hide_banner -y -i $2 \
     -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r 144 -g 144 -keyint_min 24 -hls_time 6 -hls_playlist_type vod -hls_key_info_file $PWD/output/$baseName.keyinfo -b:v 4500k -maxrate 4500k -bufsize 2250k -b:a 128k -hls_segment_filename $PWD/output/$baseName/1080pL_%03d.ts $PWD/output/$baseName/1080pL.m3u8 \
     -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r 144 -g 144 -keyint_min 24 -hls_time 6 -hls_playlist_type vod -hls_key_info_file $PWD/output/$baseName.keyinfo -b:v 6000k -maxrate 6000k -bufsize 3000k -b:a 128k -hls_segment_filename $PWD/output/$baseName/1080pH_%03d.ts $PWD/output/$baseName/1080pH.m3u8
 
-rm $PWD/output/$baseName.key
-
 echo 'Creating master playlist file'
 cat > $PWD/output/$baseName.m3u8 <<EOF
 #EXTM3U
