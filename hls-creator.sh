31a2c808b213fa3a896790738e1c73008248994f
Tobias Rath <tobias.rath@protonmail.com>
2019-09-24 13:56:21 +0200

    change script to auto generate a master playlist file

--- hls-creator.sh
+++ hls-creator.sh
@@ -10,7 +10,7 @@ then
 fi
 baseName=${fileNameArr[0]}
 
-mkdir -p $PWD/hls-output/$basename
+mkdir -p $PWD/output/$baseName
 
 ffmpeg -hide_banner -y -i $2 \
     -vf scale=w=426:h=240:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r 144 -g 144 -keyint_min 24 -hls_time 6 -hls_playlist_type vod  -b:v 500k -maxrate 500k -bufsize 300k -b:a 128k -hls_segment_filename $PWD/output/$baseName/240p_%03d.ts $PWD/output/$baseName/240p.m3u8 \
@@ -20,3 +20,22 @@ ffmpeg -hide_banner -y -i $2 \
     -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r 144 -g 144 -keyint_min 24 -hls_time 6 -hls_playlist_type vod -b:v 3500k -maxrate 3500k -bufsize 2000k -b:a 128k -hls_segment_filename $PWD/output/$baseName/720pH_%03d.ts $PWD/output/$baseName/720pH.m3u8 \
     -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r 144 -g 144 -keyint_min 24 -hls_time 6 -hls_playlist_type vod -b:v 4500k -maxrate 4500k -bufsize 2250k -b:a 128k -hls_segment_filename $PWD/output/$baseName/1080pL_%03d.ts $PWD/output/$baseName/1080pL.m3u8 \
     -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r 144 -g 144 -keyint_min 24 -hls_time 6 -hls_playlist_type vod -b:v 6000k -maxrate 6000k -bufsize 3000k -b:a 128k -hls_segment_filename $PWD/output/$baseName/1080pH_%03d.ts $PWD/output/$baseName/1080pH.m3u8
+
+cat > $PWD/output/$baseName.m3u8 <<EOF
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=500000,RESOLUTION=426x240
+$baseName/240p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1000000,RESOLUTION=640x360
+$baseName/360p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1500000,RESOLUTION=842x480
+$baseName/480p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=2500000,RESOLUTION=1280x720
+$baseName/720pL.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=3500000,RESOLUTION=1280x720
+$baseName/720pH.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=4500000,RESOLUTION=1920x1080
+$baseName/1080pL.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=6000000,RESOLUTION=1920x1080
+$baseName/1080pH.m3u8
+EOF
